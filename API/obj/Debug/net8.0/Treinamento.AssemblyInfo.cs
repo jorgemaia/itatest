e6d32bef332ebe5e84195067e4971d80c375afea
Lara Ferreira <lara.ferreira@crazytechlabs.com.br>
2024-06-23 13:18:38 -0300

    update project

--- API/obj/Debug/net8.0/Treinamento.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/Treinamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Treinamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23535a95698eae477529c1e559a93e49316abacd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Treinamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Treinamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
